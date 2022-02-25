25d4831a4992def15ad6846f8c863d4b579f88a1
Fatih Kurt <fthbtlkrt@gmail.com>
2022-02-25 13:34:55 +0100

    added tasks service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private tasksService: TasksService) {}
+    
+    @Get() 
+    getAllTasks() {
+        return ;
+    }
+
+    @Post()
+    createtask() {
+        return ;
+    }
+
+    @Get()
+    getOneTask(id: number) {
+        return `${id}`;
+    }
+
+    @Patch()
+    updateTask(id: number) {
+        return `${id}`;
+    }
+
+    @Delete()
+    deleteTask(id: number) {
+        return `${id}`
+    }
+}
